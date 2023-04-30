68459727f67fc3e9ab61b8a80aadf9b44f6b19f1
Rachana Devi <rachanadevi18@gmail.com>
2023-04-30 19:42:03 +0530

    Remove all files to start from scratch using TDD

--- src/main/java/com/example/productservice/constants/KafkaConfigConstants.java
+++
@@ -1,5 +0,0 @@
-package com.example.productservice.constants;
-
-public class KafkaConfigConstants {
-    public static final String TICKET_CREATED_TOPIC = "ticket-created-topic";
-}
--- src/main/java/com/example/productservice/exception/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.productservice.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-
-    public ProductNotFoundException(Long productId) {
-        super(String.format("Could not find product with productId {%s}", productId));
-    }
-}
-
