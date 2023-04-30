3df17ea87e7d1814ed78f90c1801c24d935f1cea
Rachana Devi <rachanadevi18@gmail.com>
2023-04-30 20:03:27 +0530

    Revert "Add failing test to see if pipeline fails"
    This reverts commit 1b05b35446bfcdb701ba0a13892c1ff97e21964e.

--- src/test/java/com/example/productservice/controller/ProductControllerTest.java
+++ src/test/java/com/example/productservice/controller/ProductControllerTest.java
@@ -1,13 +1,13 @@
 package com.example.productservice.controller;
 
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,12 +22,7 @@ public class ProductControllerTest {
         mockMvc.perform(get("/productCategories")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(result -> assertThat(result.getResponse().getContentAsString())
+                .andExpect(result -> Assertions.assertThat(result.getResponse().getContentAsString())
                         .isEqualTo("[{\"id\":1,\"name\":\"TELEVISION\"}]"));
-    }
-
-    @Test
-    void failingTest() {
-        assertThat(true).isEqualTo(false);
     }
 }
