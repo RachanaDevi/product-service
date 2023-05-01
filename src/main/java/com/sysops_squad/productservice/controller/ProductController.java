6c71f92c32f1e3cf350c8434e48ca5a217dbf6f9
Rachana Devi <rachanadevi18@gmail.com>
2023-05-01 09:04:56 +0530

    Return responseEntity for product endpoints

--- src/main/java/com/sysops_squad/productservice/controller/ProductController.java
+++ src/main/java/com/sysops_squad/productservice/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.sysops_squad.productservice.controller;
 import com.sysops_squad.productservice.model.Product;
 import com.sysops_squad.productservice.model.ProductCategory;
 import com.sysops_squad.productservice.service.ProductService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,17 +22,17 @@ public class ProductController {
 
     @GetMapping("/products/categories")
     @ResponseBody
-    public List<ProductCategory> allProductCategories() {
-        return productService.allProductCategories().stream()
+    public ResponseEntity<List<ProductCategory>> allProductCategories() {
+        return ResponseEntity.ok(productService.allProductCategories().stream()
                 .map(com.sysops_squad.productservice.entity.ProductCategory::toResponse)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 
     @GetMapping("/products/categoryName/{categoryName}")
     @ResponseBody
-    public List<Product> allProductCategories(@PathVariable String categoryName) {
-        return productService.productsHavingCategory(categoryName).stream()
+    public ResponseEntity<List<Product>> allProductCategories(@PathVariable String categoryName) {
+        return ResponseEntity.ok(productService.productsHavingCategory(categoryName).stream()
                 .map(com.sysops_squad.productservice.entity.Product::toResponse)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 }
